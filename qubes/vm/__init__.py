885363b562e9126f02c7a660f81087689a366924
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2016-03-07 01:22:27 +0100

    WIP vm: stub for new firewall API
     - introduce 'firewall-changed' event
     - add reload_firewall_for_vm stub function
    
    Should that function be private, called only from appropriate event
    handlers?
    
    QubesOS/qubes-issues#1815

--- qubes/vm/__init__.py
+++ qubes/vm/__init__.py
@@ -488,6 +488,9 @@ class BaseVM(qubes.PropertyHolder):
             subprocess.call(["sudo", "systemctl", "start",
                              "qubes-reload-firewall@%s.timer" % self.name])
 
+        # XXX any better idea? some arguments?
+        self.fire_event('firewall-changed')
+
         return True
 
     def has_firewall(self):
