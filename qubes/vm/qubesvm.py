2862d9b2957f96f36b35d42b8db05504f6e0ce27
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2016-06-26 02:28:18 +0200

    qubes/vm: remove special case for 'meminfo-writer' feature
    It should be either in plugin, or handled entirely from inside of VM.
    But surely not in main QubesVM class.
    
    Fixes QubesOS/qubes-issues#2101

--- qubes/vm/qubesvm.py
+++ qubes/vm/qubesvm.py
@@ -458,14 +458,6 @@ class QubesVM(qubes.vm.mix.net.NetVMMixin, qubes.vm.BaseVM):
 #       if not hasattr(self, 'vcpus') and not self.app.vmm.offline_mode:
 #           self.vcpus = self.app.host.no_cpus
 
-        if len(self.devices['pci']) > 0:
-            # Force meminfo-writer disabled when VM have PCI devices
-            self.features['meminfo-writer'] = None
-        elif not isinstance(self, qubes.vm.adminvm.AdminVM) \
-                and 'meminfo-writer' not in self.features:
-            # Always set if meminfo-writer should be active or not
-            self.features['meminfo-writer'] = '1'
-
         if xml is None:
             # new qube, disable updates check if requested for new qubes
             # SEE: 1637 when features are done, migrate to plugin
