490f62ac30cb988190b68551ea7b0389fab79e15
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2017-03-20 22:54:03 +0100

    tests: qubespolicy tests
    Fixes QubesOS/qubes-issues#2460

--- qubes/tests/__init__.py
+++ qubes/tests/__init__.py
@@ -897,6 +897,7 @@ def load_tests(loader, tests, pattern): # pylint: disable=unused-argument
             'qubes.tests.tools.qvm_device',
             'qubes.tests.tools.qvm_firewall',
             'qubes.tests.tools.qvm_ls',
+            'qubespolicy.tests',
             ):
         tests.addTests(loader.loadTestsFromName(modname))
 
